bf11951e39c15bc619dd7d754a29c87de4afb8b4
Vincent Roma <roma.vincent@gmail.com>
2023-04-12 21:44:48 -0400

    android app + server

--- App.js
+++ App.js
@@ -1,8 +1,12 @@
+import { AppRegistry } from 'react-native';
+import { name as appName } from './app.json';
 import * as React from 'react';
 import {Platform, Text, View, StyleSheet, Button } from 'react-native';
 import { Audio } from 'expo-av';
 import * as Speech from 'expo-speech';
 import RNFetchBlob from 'rn-fetch-blob';
+
+AppRegistry.registerComponent(appName, () => App);
 
 export default function App() {
   const [recording, setRecording] = React.useState();
@@ -13,43 +17,44 @@ export default function App() {
 
   async function postFileToAPI(fileUri) {
     try {
-      const formData = new FormData();
+      if(fileUri){
+        const formData = new FormData();
   
-      // Get file type and name
-      const fileTypeMatch = /\.([a-zA-Z]+)$/i.exec(fileUri);
-      const fileType = fileTypeMatch ? fileTypeMatch[1] : 'unknown';
-      const fileName = `file.${fileType}`;
-  
-      // Handle the file differently based on the platform
-      let file = {};
-      if (Platform.OS === 'ios') {
-        file = {
-          uri: fileUri,
-          type: `image/${fileType}`,
-          name: fileName,
-        };
-      } else {
-        // Android requires using RNFetchBlob to get the file data
-        const fileData = await RNFetchBlob.fs.readFile(fileUri, 'base64');
-        file = {
-          uri: `data:image/${fileType};base64,${fileData}`,
-          type: `image/${fileType}`,
-          name: fileName,
-        };
+        // Get file type and name
+        const fileTypeMatch = /\.([a-zA-Z]+)$/i.exec(fileUri);
+        const fileType = fileTypeMatch ? fileTypeMatch[1] : 'unknown';
+        const fileName = `file.${fileType}`;
+    
+        // Handle the file differently based on the platform
+        let file = {};
+        if (Platform.OS === 'ios') {
+          file = {
+            uri: fileUri,
+            type: `image/${fileType}`,
+            name: fileName,
+          };
+        } else {
+          // Android requires using RNFetchBlob to get the file data
+          const fileData = await RNFetchBlob.fs.readFile(fileUri, 'base64');
+          file = {
+            uri: `data:image/${fileType};base64,${fileData}`,
+            type: `image/${fileType}`,
+            name: fileName,
+          };
+        }
+    
+        formData.append('file', file);
+    
+        const response = await fetch('https://your-rest-api-url.com/upload', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+          body: formData,
+        });
+        const responseData = await response.json();
+        console.log('File uploaded successfully:', responseData);
       }
-  
-      formData.append('file', file);
-  
-      const response = await fetch('https://your-rest-api-url.com/upload', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'multipart/form-data',
-        },
-        body: formData,
-      });
-  
-      const responseData = await response.json();
-      console.log('File uploaded successfully:', responseData);
     } catch (error) {
       console.error('Error uploading file:', error);
     }
@@ -81,7 +86,7 @@ export default function App() {
     await Audio.setAudioModeAsync({
       allowsRecordingIOS: false,
     });
-    const uri = recording.getURI();
+    const uri = await recording.getURI();
     console.log('Recording stopped and stored at', uri);
     response = postFileToAPI(uri);
     console.log("API response: ", response)
